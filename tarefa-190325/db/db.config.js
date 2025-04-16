204b713f3d57e9ad90b790b44c27829f1fefeabe
João Victor Sant'Ana Cortabitart <joaovictorcortabitart@gmail.com>
2025-04-16 16:14:07 -0300

    db

--- tarefa-190325/db/db.config.js
+++ tarefa-190325/db/db.config.js
@@ -1,6 +1,29 @@
 const mySql = require('mysql2')
 
 const connection = mySql.createConnection({
+    host: "localhost",
+    //usar sudo docker inspect moodle-atv ip-address
+    user: 'root',
+    port: 3306,
+    password: '12345',
+    database: 'moodle'
+})
+
+connection.connect((err)=> {
+    if (err) {
+        console.error("Erro ao conectar ao banco de dados: ", err)
+        return
+    }
+
+    console.log("Conexão efetuada")
+})
+
+module.exports = connection;
+
+/*
+const mySql = require('mysql2')
+
+const connection = mySql.createConnection({
     host: "172.17.0.2",
     //usar sudo docker inspect moodle-atv ip-address
     user: 'root',
@@ -19,3 +42,4 @@ connection.connect((err)=> {
 })
 
 module.exports = connection;
+*/
