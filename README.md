6bc5d80b84bd3716c1f6b68d07feea0ec5efa9b5
João Victor Sant'Ana Cortabitart <joaovictorcortabitart@gmail.com>
2025-04-20 00:06:24 -0300

    readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # trabalhoBrasil
+
+Para instalar o projeto, siga os passos abaixo:
+1- Clone o repositório:
+GIT CLONE https://github.com/J-V-S-C/trabalhoBrasil.git
+
+2- Acesse o diretório do projeto
+cd trabalhoBrasil
+
+3- Instale as dependências:
+npm install
+
+4- Crie um arquivo .env na raiz do projeto, e adicione as variáveis de ambiente necessárias.
+
+5- Crie um banco de dados no MySql, adicione as credenciais no arquivo .env e copie e cole o conteúdo do arquivo mysql.sql no mysql workbanch ou no CLI do mysql
+exemplo de credenciais:
+HOST: "meu host",
+USER: 'meu user',
+PORT: 1111,
+PASSWORD: 'minha senha',
+DATABASE: 'meu nome do database'
+
+6- Inicie o servidor:
+npm run start
+
+7- Acesse o projeto no navegador:
+http://localhost:3000
--- db/db.config.js
+++ db/db.config.js
@@ -1,12 +1,12 @@
 const mySql = require('mysql2')
 
 const connection = mySql.createConnection({
-    host: "172.17.0.2",
+    host: process.env.HOST,
     //usar sudo docker inspect moodle-atv ip-address
-    user: 'root',
-    port: 3306,
-    password: '12345678',
-    database: 'sgpBrasil'
+    user: process.env.ROOT,
+    port: process.env.PORT,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
 })
 
 connection.connect((err)=> {
@@ -21,29 +21,10 @@ connection.connect((err)=> {
 module.exports = connection;
 
 /*
-
-
-
-const mySql = require('mysql2')
-
-const connection = mySql.createConnection({
     host: "localhost",
     //usar sudo docker inspect moodle-atv ip-address
     user: 'root',
     port: 3306,
     password: '12345',
     database: 'moodle'
-})
-
-connection.connect((err)=> {
-    if (err) {
-        console.error("Erro ao conectar ao banco de dados: ", err)
-        return
-    }
-
-    console.log("Conexão efetuada")
-})
-
-module.exports = connection;
-
 */
